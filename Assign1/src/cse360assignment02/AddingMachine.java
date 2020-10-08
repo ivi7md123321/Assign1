f0182ab4213c2c3b3c508096a8efcab9aa11a0e2
ivi7md123321 <55615514+ivi7md123321@users.noreply.github.com>
2020-10-07 23:33:09 -0700

    First time committing test

---
+++ Assign1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,33 @@
+/*Student Name: Mohammed Alramadan
+ *Class Description: This class represents the action of adding new machines to a set of machines.
+ * 
+ * 
+ */
+
+package cse360assignment02;
+
+public class AddingMachine {
+	
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
+
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
