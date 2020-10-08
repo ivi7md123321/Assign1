49a74ec7457807ad0a05c3256320dddc13a95951
ivi7md123321 <55615514+ivi7md123321@users.noreply.github.com>
2020-10-07 23:51:15 -0700

    Defined all methods

--- Assign1/src/cse360assignment02/AddingMachine.java
+++ Assign1/src/cse360assignment02/AddingMachine.java
@@ -1,5 +1,5 @@
 /*Student Name: Mohammed Alramadan
- *Class Description: This class represents the action of adding new machines to a set of machines.
+ *Class Description: This class represents the an adding machin that would do calculations and store them.
  * 
  * 
  */
@@ -8,26 +8,41 @@ package cse360assignment02;
 
 public class AddingMachine {
 	
+//attributes
   private int total;
-  
+  private String history;
+
+//constructor
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "0 ";
   }
   
+ //returns the the value of the variable total
   public int getTotal () {
-    return 0;
+    return total;
   }
   
+//adds a value to the total
   public void add (int value) {
+	  total += value;
+	  history+=" + "+value;
   }
 
+//subtracts a value to the total
   public void subtract (int value) {
+	  total -= value;
+	  history+=" - "+value;
   }
 
+//Prints a history of all calculations
   public String toString () {
-    return "";
+    return history;
   }
 
+//resets all attributes
   public void clear() {
+	  total = 0;
+	  history ="0 ";
   }
 }
